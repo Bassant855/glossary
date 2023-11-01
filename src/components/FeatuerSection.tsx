b58549c6938e49d7e4efe9e63eb0fc15bde7a022
Bassant855 <bassanthassan855@gmail.com>
2023-11-01 08:25:12 +0200

    some change

--- src/components/FeatuerSection.tsx
+++ src/components/FeatuerSection.tsx
@@ -1,18 +1,8 @@
-
-
-
-const data = [
-    {id:0, img:'../../public/assets/product__1.webp',name:'Dried Mango', price:'$500'},
-    {id:1, img:'../../public/assets/product__2.webp',name:'Crunchy Crisps', price:'$300'},
-    {id:2, img:'../../public/assets/product__3.webp',name:'Jewel Cranberries', price:'$200'},
-    {id:3, img:'../../public/assets/product__4.webp',name:'Almond organic', price:'$100'},
-]
-const data2 = [
-    {id:0, img:'../../public/assets/product__5.webp',name:'Fresh Tomato', price:'$400'},
-    {id:1, img:'../../public/assets/product__2.webp',name:'Crunchy Crisps', price:'$300'},
-    {id:2, img:'../../public/assets/product__3.webp',name:'Jewel Cranberries', price:'$200'},
-    {id:3, img:'../../public/assets/product__1.webp',name:'Almond organic', price:'$100'},
-]
+import product1 from '../../public/assets/product__1.webp';
+import product2 from '../../public/assets/product__2.webp';
+import product3 from '../../public/assets/product__3.webp';
+import product4 from '../../public/assets/product__4.webp';
+import product5 from '../../public/assets/product__5.webp';
 import feature1 from '../../public/assets/feature__1.webp'
 import feature2 from '../../public/assets/feature__2.webp'
 import {AiFillStar} from 'react-icons/ai'
@@ -44,33 +34,95 @@ const FeatuerSection = () => {
                 <img src={feature1} alt="" className='w-full h-full object-cover' />
             </div>
 
-            {
-                data.map((pro1) => {
-                    return (
-                        <div key={pro1.id} className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
-                            <img src={pro1.img} alt="" />
+         
+            <div className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product1} alt="" />
 
-                            <div className='flex justify-between px-3 relative'>
-                                <div className='flex text-yellow-400 text-xl'>
-                                    <AiFillStar/>
-                                    <AiFillStar/>
-                                    <AiFillStar/>
-                                    <AiFillStar/>
-                                    <AiOutlineStar/>
-                                </div>
-                                <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
-                                 flex items-center justify-center -top-4 absolute right-2' >
-                                    <AiOutlineShopping/>
-                                </button>
-                                
-                            </div>
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
 
-                            <h3 className='font-medium text-md p-3'>{pro1.name}</h3>
-                            <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>{pro1.price}</p>
-                        </div>
-                    )
-                })
-            }
+                <h3 className='font-medium text-md p-3'>Dried Mango</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$500</p>
+            </div>
+
+            <div className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product2} alt="" />
+
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
+
+                <h3 className='font-medium text-md p-3'>Crunchy Crisps</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$300</p>
+            </div>
+
+            <div className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product3} alt="" />
+
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
+
+                <h3 className='font-medium text-md p-3'>Jewel Cranberries</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$200</p>
+            </div>
+
+            <div className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product4} alt="" />
+
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
+
+                <h3 className='font-medium text-md p-3'>Almond organic</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$100</p>
+            </div>
+                   
         </div>
 
 
@@ -91,33 +143,94 @@ const FeatuerSection = () => {
                 <img src={feature2} alt="" className='w-full h-full object-cover' />
             </div>
 
-            {
-                data2.map((pro) => {
-                    return (
-                        <div key={pro.id} className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
-                            <img src={pro.img} />
+            <div  className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product5} />
 
-                            <div className='flex justify-between px-3 relative'>
-                                <div className='flex text-yellow-400 text-xl'>
-                                    <AiFillStar/>
-                                    <AiFillStar/>
-                                    <AiFillStar/>
-                                    <AiFillStar/>
-                                    <AiOutlineStar/>
-                                </div>
-                                <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
-                                 flex items-center justify-center -top-4 absolute right-2' >
-                                    <AiOutlineShopping/>
-                                </button>
-                                
-                            </div>
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
 
-                            <h3 className='font-medium text-md p-3'>{pro.name}</h3>
-                            <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>{pro.price}</p>
-                        </div>
-                    )
-                })
-            }
+                <h3 className='font-medium text-md p-3'>Fresh Tomato</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$400</p>
+            </div>
+
+            <div className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product1} alt="" />
+
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
+
+                <h3 className='font-medium text-md p-3'>Dried Mango</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$500</p>
+            </div>
+
+            <div className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product2} alt="" />
+
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
+
+                <h3 className='font-medium text-md p-3'>Crunchy Crisps</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$300</p>
+            </div>
+
+            <div className='border border-gray-300 transition-all cursor-pointer hover:scale-105 rounded-lg'>
+                <img src={product3} alt="" />
+
+                <div className='flex justify-between px-3 relative'>
+                    <div className='flex text-yellow-400 text-xl'>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <button className='bg-accent text-white w-[50px] h-[50px] rounded-full text-[28px]
+                        flex items-center justify-center -top-4 absolute right-2' >
+                        <AiOutlineShopping/>
+                    </button>
+                    
+                </div>
+
+                <h3 className='font-medium text-md p-3'>Jewel Cranberries</h3>
+                <p className='text-red-500 px-3 text-xl pb-3 font-semibold'>$200</p>
+            </div>
+        
         </div>
 
 
